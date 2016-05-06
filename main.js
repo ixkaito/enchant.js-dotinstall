618989ba4b4aa6d5de0c961e6c753f7f4cd8f934
Kite <ixkaito@gmail.com>
2016-05-07 07:24:26 +0900

    Display labels

--- main.js
+++ main.js
@@ -13,23 +13,25 @@ window.onload = function() {
     bear.y = 0;
     bear.frame = 0;
 
-    bear.addEventListener('enterframe', function() {
+    bear.on('enterframe', function() {
       if (core.input.left) this.x -= 5;
       if (core.input.right) this.x += 5;
       if (core.input.up) this.y -= 5;
       if (core.input.down) this.y += 5;
     });
 
-    bear.on('touchstart', function() {
-      core.rootScene.removeChild(this);
-    });
-
-    core.rootScene.on('touchstart', function(e) {
-      bear.x = e.x;
-      bear.y = e.y;
+    var label = new Label();
+    label.x = 280;
+    label.y = 5;
+    label.color = 'red';
+    label.font = '14px "Arial"';
+    label.text = '0';
+    label.on('enterframe', function() {
+      label.text = (core.frame / core.fps).toFixed(2);
     });
 
     core.rootScene.addChild(bear);
+    core.rootScene.addChild(label);
 
   };
   core.start();
